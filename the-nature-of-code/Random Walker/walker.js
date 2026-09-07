1648ce64c841650f753d4d6e9427d06e10639fb7
remay <rem_ay@hotmail.fr>
2026-09-07 19:54:26 +1200

    documentation added to get auto completion.

--- the-nature-of-code/Random Walker/walker.js
+++ the-nature-of-code/Random Walker/walker.js
@@ -1,18 +1,36 @@
+/**
+ * A Walker class that performs a random walk on the canvas
+ */
 class Walker {
+  /**
+   * Creates a new Walker at the center of the canvas
+   * @param {number} x - The x position (not used, walker starts at canvas center)
+   * @param {number} y - The y position (not used, walker starts at canvas center)
+   * @param {p5.Color} color - The color of the walker
+   */
   constructor(x, y, color) {
+    /** @type {number} */
     this.x = width / 2;
+    /** @type {number} */
     this.y = height / 2;
+    /** @type {p5.Color} */
     this.color = color;
   }
 
+  /**
+   * Displays the walker as a point on the canvas
+   */
   show() {
     stroke(this.color);
     strokeWeight(2);
     point(this.x, this.y);
   }
 
+  /**
+   * Moves the walker one step in a random direction
+   * Randomly chooses: 0=right, 1=left, 2=down, 3=up
+   */
   step() {
-    //{!1} 0, 1, 2, or 3. The random choice determines the step.
     let choice = floor(random(4));
     if (choice === 0) {
       this.x++;
--- the-nature-of-code/Random Walker/walker_0.1.js
+++ the-nature-of-code/Random Walker/walker_0.1.js
@@ -1,5 +1,13 @@
-// Walker with a tendency to go towards the right
+/**
+ * A Walker that has a tendency to move towards the right
+ * Extends the Walker class and overrides the step() method with weighted probabilities
+ * @extends Walker
+ */
 class Walker_01 extends Walker {
+  /**
+   * Moves the walker one step with a bias towards moving right (40% chance)
+   * Probabilities: 40% right, 20% left, 20% down, 20% up
+   */
   step() {
     let r = random(1);
 
