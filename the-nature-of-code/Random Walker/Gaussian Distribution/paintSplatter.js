1a59dc161077601d046c8c9ea89f7fe3f2589d65
remay <rem_ay@hotmail.fr>
2026-09-08 19:47:32 +1200

    paint created. sketch initialized with a slider allowing the control of the paint's size.

--- the-nature-of-code/Random Walker/Gaussian Distribution/paintSplatter.js
+++ the-nature-of-code/Random Walker/Gaussian Distribution/paintSplatter.js
@@ -1,3 +1,24 @@
+/**
+ * A PaintSplatter class that represents a paint splatter at a given position
+ */
 class paintSplatter {
-    
+  /**
+   * Creates a new paint splatter at the specified position
+   * @param {number} x - The x position of the splatter
+   * @param {number} y - The y position of the splatter
+   */
+  constructor(x, y, w, h) {
+    /** @type {number} */
+    this.x = x;
+    /** @type {number} */
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+
+  show() {
+    noStroke();
+    fill(green);
+    ellipse(this.x, this.y, this.w, this.h);
+  }
 }
--- the-nature-of-code/Random Walker/Gaussian Distribution/sketch.js
+++ the-nature-of-code/Random Walker/Gaussian Distribution/sketch.js
@@ -1,7 +1,21 @@
-function setup() {
+let width = 640;
+let height = 480;
 
+let paint;
+function setup() {
+  createCanvas(width, height);
+  background(255);
+  createControls();
 }
 
 function draw() {
-    
+  let g = sizeSlider.value();
+  paint = new paintSplatter(random(width), random(height), g, g);
+  paint.show();
+}
+
+let sizeSlider;
+function createControls() {
+  sizeSlider = createSlider(5, 50, 20, 0);
+  createP("Paint Splatter Simulation");
 }
